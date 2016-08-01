696503aa334c38af48a30eb2e3ec8986b0909e4e
Jeff Posey <jeff.posey@medseek.com>
2016-08-01 13:26:31 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,3 +2,6 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 setx /M ChocolateyInstall e:\ProgramData\chocolatey
+
+choco install -Y git
+
