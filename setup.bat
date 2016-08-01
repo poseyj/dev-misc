ebac38506de54d2cb1105e11d25fa9e49a22edd9
Jeff Posey <jeff.posey@medseek.com>
2016-08-01 13:11:16 -0500

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,4 @@
+
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+setx /M ChocolateyInstall e:\ProgramData\chocolatey
